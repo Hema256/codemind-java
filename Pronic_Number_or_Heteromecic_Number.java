f4f4e5ed7f1a9b9c33c6eacfb984f83eae87ec11
Hema256 <84697657+Hema256@users.noreply.github.com>
2022-10-03 13:17:41 +0530

    import java.util.Scanner; class abn {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n,c=0;         n=sc.nextInt();         for(int i=0; i<n; i++)         {             if(i*(i+1)==n)             {                 c=1;                 break;             }         }         if(c==1)         System.out.print("YES");         else         System.out.print("NO");     } }

--- Pronic_Number_or_Heteromecic_Number.java
+++ Pronic_Number_or_Heteromecic_Number.java
@@ -1,26 +1,22 @@
 import java.util.Scanner;
-
-class Example13 {
-
+class abn
+{
     public static void main(String args[])
-       {
-        Scanner sc = new Scanner(System.in);
-        
-        int num = sc.nextInt();
-        int ans = 0;
-    
-        for(int i=0; i<num; i++)
+    {
+        Scanner sc=new Scanner(System.in);
+        int n,c=0;
+        n=sc.nextInt();
+        for(int i=0; i<n; i++)
         {
-            if(i*(i+1) == num)
+            if(i*(i+1)==n)
             {
-                ans = 1;
+                c=1;
                 break;
             }
         }
-         
-        if(ans == 1)
-            System.out.println("YES");
+        if(c==1)
+        System.out.print("YES");
         else
-            System.out.println("NO");      
+        System.out.print("NO");
     }
 }
