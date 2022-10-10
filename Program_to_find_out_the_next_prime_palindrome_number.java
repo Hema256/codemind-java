4f42436298998cfa03badc8d7d843ae2d4182101
Hema256 <84697657+Hema256@users.noreply.github.com>
2022-10-10 09:25:57 +0530

    import java.util.Scanner; class PrimePalindrome {     public static boolean isPalindrome(int n)     {        int temp=n;         int sum=0,r;          while(n>0)           {                  r=n%10;                  sum=sum*10 + r;                  n= n/10;          }          if(temp==sum)                   return true;          else                   return false;     }     public static boolean isPrime(int n)     {         int count=0;         for(int i=2;i<=(int)Math.sqrt(n);i++)          {              if(n%i==0)                 count++;          }          if(count==0)              return true;          else              return false;     }     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n,i;
    		n=sc.nextInt();
            for(i=n+1; ;i++)
            {
                if(isPrime(i) && isPalindrome(i))
                  {
                      System.out.println(i);
    				  break;
                  }
            }      
        }
    }

--- Program_to_find_out_the_next_prime_palindrome_number.java
+++ Program_to_find_out_the_next_prime_palindrome_number.java
@@ -1,52 +1,47 @@
 import java.util.Scanner;
-class ab
+class PrimePalindrome
 {
-    public static boolean ispalindrome(int n)
+    public static boolean isPalindrome(int n)
     {
-        int r,sum=0;
-        int temp=n;
-        while(n>0)
-        {
-            r=n%10;
-            sum=sum*10+r;
-            n=n/10;
-            
-        }
-        if (temp==sum)
-            return true;
-        else
-            return false;
+       int temp=n;
+        int sum=0,r;
+         while(n>0)
+          {
+                 r=n%10;
+                 sum=sum*10 + r;
+                 n= n/10;
+         }
+         if(temp==sum)
+                  return true;
+         else
+                  return false;
     }
-    public static boolean isprime(int n)
+    public static boolean isPrime(int n)
     {
         int count=0;
-        for( int i=2; i<=(int)Math.sqrt(n);i++)
-        {
-            if(n%i==0)
+        for(int i=2;i<=(int)Math.sqrt(n);i++)
+         {
+             if(n%i==0)
                 count++;
-        }
-        if(count==0)
-            return true;
-        else
-            return false;
-        
+         }
+         if(count==0)
+             return true;
+         else
+             return false;
     }
-    
     public static void main(String args[])
     {
         Scanner sc=new Scanner(System.in);
         int n,i;
-        n=sc.nextInt();
-        for(i=n+1; ; i++)
+      
+		n=sc.nextInt();
+        for(i=n+1; ;i++)
         {
-            if(isprime(i) && ispalindrome(i))
-            {
-                System.out.println(i);
-                break;
-            }
-            
-        }
-        
-        
+            if(isPrime(i) && isPalindrome(i))
+              {
+                  System.out.println(i);
+				  break;
+              }
+        }      
     }
 }
