c26471989363c874b6f251930f1e08abfc3c2af1
Hema256 <84697657+Hema256@users.noreply.github.com>
2022-10-15 13:59:00 +0530

    import java.util.Scanner; class count {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int x[], n,i,c=0;         n=sc.nextInt();         x=new int[n];         for( i=0; i<n; i++)         {             x[i]=sc.nextInt();
            }
            for(i=1; i<n-1; i++)
            {
                if(x[i]%2==0)
                {
                    if(x[i-1]%2==0 && x[i+1]%2==0 ||x[i-1]%2==0 && x[i+1]%2==0)
                    c++;
                }
            }
            System.out.print(c);
        }
    }

---
+++ count_even_numbers_between_even_numbers.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+class count
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int x[], n,i,c=0;
+        n=sc.nextInt();
+        x=new int[n];
+        for( i=0; i<n; i++)
+        {
+            x[i]=sc.nextInt();
+            
+        }
+        for(i=1; i<n-1; i++)
+        {
+            if(x[i]%2==0)
+            {
+                if(x[i-1]%2==0 && x[i+1]%2==0 ||x[i-1]%2==0 && x[i+1]%2==0)
+                c++;
+            }
+        }
+        System.out.print(c);
+    }
+}
