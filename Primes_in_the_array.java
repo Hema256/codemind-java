3f32300302d22180f14e89e839acccc42233b5e8
Hema256 <84697657+Hema256@users.noreply.github.com>
2022-10-26 18:06:07 +0530

    import java.util.*; class prime {     public static boolean isprime(int n)     {         int count=0;         if(n==1)         return false;         for( int i=2; i<=(int)Math.sqrt(n); i++)         {             if(n%i==0)             count++;         }         if(count==0)         {             return true;         }         else         {             return false;         }     }     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n,i,c=0;         n=sc.nextInt();         int x[]=new int[n];         for(i=0; i<n; i++)         {             x[i]=sc.nextInt();         }         for(i=0; i<n; i++)         {             if(isprime(x[i]))             {                 c++;             }         }         System.out.print(c);     }
    }

--- Primes_in_the_array.java
+++ Primes_in_the_array.java
@@ -1,40 +1,43 @@
-import java.util.Scanner;
-class ad
+import java.util.*;
+class prime
 {
     public static boolean isprime(int n)
     {
-        if (n<2)
-        return false;
         int count=0;
-        for(int i=2;i<=(int)Math.sqrt(n);i++)
+        if(n==1)
+        return false;
+        for( int i=2; i<=(int)Math.sqrt(n); i++)
         {
-            if (n%i==0)
+            if(n%i==0)
             count++;
         }
-        if (count==0)
-        return true;
+        if(count==0)
+        {
+            return true;
+        }
         else
-        return false;
+        {
+            return false;
+        }
     }
     public static void main(String args[])
     {
-        int x[],n,i,c=0;
         Scanner sc=new Scanner(System.in);
+        int n,i,c=0;
         n=sc.nextInt();
-        x=new int[n];
-        
+        int x[]=new int[n];
         for(i=0; i<n; i++)
-        x[i]=sc.nextInt();
+        {
+            x[i]=sc.nextInt();
+        }
         for(i=0; i<n; i++)
         {
             if(isprime(x[i]))
             {
-                
                 c++;
-                
             }
         }
-        System.out.println(c);
-        
+        System.out.print(c);
     }
+    
 }
