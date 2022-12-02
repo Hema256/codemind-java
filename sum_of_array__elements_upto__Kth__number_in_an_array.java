a3d232c7d910aa36df8ecf8ca9de3127761c2e91
Hema256 <84697657+Hema256@users.noreply.github.com>
2022-12-02 23:05:53 +0530

    import java.util.Scanner; class average {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n,x[],i,sum=0,k;         double avg=0;         n=sc.nextInt();         x=new int[n];         for(i=0; i<n; i++)             x[i]=sc.nextInt();         k=sc.nextInt();         for(i=0; i<k; i++)         {             sum=sum+x[i];         }         System.out.print(sum);
        }
        
    }

---
+++ sum_of_array__elements_upto__Kth__number_in_an_array.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+class average
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n,x[],i,sum=0,k; 
+        double avg=0;
+        n=sc.nextInt();
+        x=new int[n];
+        for(i=0; i<n; i++)
+            x[i]=sc.nextInt();
+        k=sc.nextInt();
+        for(i=0; i<k; i++)
+        {
+            sum=sum+x[i];
+        }
+        System.out.print(sum);
+            
+    }
+    
+}
